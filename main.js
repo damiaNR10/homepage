249494b1d0abf47f016bd0a3391e9f95f175be92
damiaNR10 <damianrog96@gmail.com>
2020-05-10 22:12:16 +0200

    added bem class

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const name = 'John';
+const name = "John";
 const age = 27;
 
 console.log(`Cześć, mam na imie ${name} i mam ${age} lat`);
 
+const heading = document.querySelector('.main-heading--js');
+heading.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
