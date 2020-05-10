2943a4966f3f1076619a2cfa4fb5259835bba0e8
damiaNR10 <damianrog96@gmail.com>
2020-05-10 23:02:37 +0200

    whole project bem classes

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ const age = 27;
 
 console.log(`Cześć, mam na imie ${name} i mam ${age} lat`);
 
-const heading = document.querySelector('.main-heading--js');
-heading.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
+// const heading = document.querySelector('.main-heading--js');
+// heading.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
+
+const content = document.querySelector('.main-section__description--js');
+content.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
