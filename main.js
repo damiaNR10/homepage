211b71fd934e71372fd2ec36ac5b9d24a79d5c6d
damiaNR10 <damianrog96@gmail.com>
2020-05-14 23:12:39 +0200

    new content - lists and table

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const name = "John";
 const age = 27;
 
-console.log(`Cześć, mam na imie ${name} i mam ${age} lat`);
+// console.log(`Cześć, mam na imie ${name} i mam ${age} lat`);
 
-// const heading = document.querySelector('.main-heading--js');
-// heading.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
+//const emptyParagraph = document.querySelector('.main-section__description--js');
+//emptyParagraph.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
 
-const content = document.querySelector('.main-section__description--js');
-content.innerHTML = `Cześć, mam na imie ${name} i mam ${age} lat`;
+// function greet(age, name) {
+//     return `Cześć, mam na imie ${name} i mam ${age} lat`;
+// }
+
+//Notacja fat arrow function
+const greet = (age, name) => {
+    console.log(`Cześć, mam na imie ${name} i mam ${age} lat`);
+}
+
+greet(age, name);
+
+function createContent(selector, content) {
+    const emptySelector = document.querySelector(selector);
+    emptySelector.innerHTML = content;
+}
+
+createContent('.main-section__description--js', 'Hello World!');
